49b05063e098196edcaa0436e9e7a4d8b294fe00
HyunjinKim <koujie11@gmail.com>
2024-02-20 00:02:00 +0900

    feat : mypaga router 연결

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -4,6 +4,7 @@ import Layout from "components/layout/Layout";
 import LetterProvider from "context/Context";
 import DetailPage from "pages/DetailPage";
 import MainPage from "pages/MainPage";
+import Mypage from "pages/Mypage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
@@ -16,6 +17,7 @@ const Router = () => {
             <Route path="/detail/:id" element={<DetailPage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/mypage" element={<Mypage />} />
           </Routes>
         </Layout>
       </LetterProvider>
