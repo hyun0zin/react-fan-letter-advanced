f814c2fe4408977c16388faba0680b826b93213f
HyunjinKim <koujie11@gmail.com>
2024-02-19 19:56:52 +0900

    fix : context api 멤버별 imageURL 추가

--- src/context/Context.js
+++ src/context/Context.js
@@ -32,6 +32,15 @@ const LetterProvider = ({ children }) => {
     setLetters(updatedLetters);
   };
 
+  //멤버별 imageUrl
+  const memberImagesData = {
+    JENNIE:
+      "https://i.pinimg.com/736x/9a/1c/b8/9a1cb83f3c42b75b55caffd76c9b5b96.jpg",
+    JISOO:
+      "https://i.pinimg.com/564x/7d/85/27/7d8527048832709dea1c4c5a04123921.jpg",
+    ROSÉ: "https://i.pinimg.com/564x/f5/4d/82/f54d828a86423aff30473d263ea9742f.jpg",
+    LISA: "https://i.pinimg.com/564x/0b/88/da/0b88da206ad13b0974b783cb6f6c6bcc.jpg",
+  };
   return (
     <Context.Provider
       value={{
@@ -40,6 +49,7 @@ const LetterProvider = ({ children }) => {
         removeBtn,
         updateBtn,
         updatedLetters,
+        memberImagesData,
       }}
     >
       {children}
